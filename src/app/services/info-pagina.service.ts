fb9ab357ac2e39a685be420c8a6f1af64cc7064a
Nicolas Gravertt <nicolasgravertt@hotmail.com>
2019-03-14 02:23:36 -0300

    Fin seccion 4

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { infoPagina } from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+
+  info: infoPagina = {};
+  cargada = false;
+
+  constructor(private http: HttpClient) {
+
+    //console.log('Servicio info pagina listo');
+
+    //leer el archivo JSON
+    this.http.get('assets/data/data-pagina.json')
+      .subscribe( (resp:infoPagina) =>{
+
+        this.cargada = true;
+        this.info = resp;
+        //console.log(resp['twitter']);
+        console.log(resp);
+
+      });
+
+   }
+}
